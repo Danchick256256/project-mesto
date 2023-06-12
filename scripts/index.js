bed6e9534d005a95f04296e5ce2f421b095c4af4
Danchick256256 <danilfpronkin280504@gmail.com>
2023-06-12 14:10:51 +0300

    new version

--- scripts/index.js
+++ scripts/index.js
@@ -29,6 +29,7 @@ const newPlaceNameInput = newPlaceElement.querySelector('input[name="name"]');
 const newPlaceLinkInput = newPlaceElement.querySelector('input[name="link"]');
 
 function addListenersToCards() {
+    console.log(`{adding.listeners.to.new.cards}`);
     const deleteButtons = document.getElementsByClassName("card__button-delete");
     for (const deleteButton of deleteButtons) {
         deleteButton.addEventListener('click', handleDeleteClick);
@@ -45,6 +46,7 @@ function addListenersToCards() {
 
 document.addEventListener('keydown', (event) => {
     if (event.code === "Escape") {
+        console.log(`{escape.pressed}`);
         for (const popup of popups) {
             closePopup(popup);
         }
@@ -60,24 +62,29 @@ for (const closeButton of closeButtons) {
 }
 
 editButton.addEventListener('click', () => {
+    console.log(`{click.edit.button}`);
     openPopup(editPopup);
 });
 
 addButton.addEventListener('click', () => {
+    console.log(`{click.add.button}`);
     openPopup(newPlacePopup);
 });
 
 const closePopup = (popup) => {
-    popup.classList.remove('popup_opened')
+    console.log(`{close.popup{${popup}}`);
+    popup.classList.remove('popup_opened');
 };
 
 const openPopup = (popup) => {
-    popup.classList.add('popup_opened')
+    console.log(`{open.popup{${popup}}`);
+    popup.classList.add('popup_opened');
 };
 
 const addInitialCard = () => {
     for (const initialCard of initialCards) {
         const card = new Card(initialCard.link, initialCard.name, '.card__template').createCard();
+        console.log(`{adding.initial.cards{${initialCard.link}, ${initialCard.name}}`);
         cardsSection.prepend(card);
     }
     addListenersToCards();
@@ -86,6 +93,7 @@ const addInitialCard = () => {
 addInitialCard();
 
 function handleEditFormSubmit(evt) {
+    console.log(`{submit.edit.form}`);
     evt.preventDefault();
     nameInput.setAttribute("value", nameInput.value);
     jobInput.setAttribute("value", jobInput.value);
@@ -95,6 +103,7 @@ function handleEditFormSubmit(evt) {
 }
 
 function handleNewPlaceFormSubmit(evt) {
+    console.log(`{submit.newPlace.form}`);
     evt.preventDefault();
     const card = new Card(newPlaceLinkInput.value, newPlaceNameInput.value, '.card__template').createCard();
     cardsSection.prepend(card);
@@ -103,16 +112,20 @@ function handleNewPlaceFormSubmit(evt) {
 }
 
 function handleImageClick(evt) {
+    const imageCaption = evt.target.nextElementSibling.getElementsByClassName("card__title")[0].innerHTML;
+    console.log(`{click.on.image{caption: ${imageCaption}}`);
     image.setAttribute("src", evt.target.getAttribute("src"));
-    caption.innerHTML = evt.target.nextElementSibling.getElementsByClassName("card__title")[0].innerHTML;
+    caption.innerHTML = imageCaption;
     openPopup(imagePopup);
 }
 
 function handleLikeClick(evt) {
+    console.log(`{handled.like.click}`);
     evt.target.classList.toggle("card__button-like_active_true");
 }
 
 function handleDeleteClick(evt) {
+    console.log(`{handled.remove.click}`);
     evt.target.parentNode.classList.add("card__remove");
     setTimeout(() => evt.target.parentNode.remove(), 350);
 }
